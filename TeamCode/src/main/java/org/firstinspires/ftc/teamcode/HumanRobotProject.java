b9fd603d32bbe9b194680a27e272da13a5960482
RazrSlyr <sharpedo75@gmail.com>
2017-07-16 12:38:32 -0700

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HumanRobotProject.java
@@ -0,0 +1,103 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="LOL This doesnt really matter so u can ignore this XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD", group="Neither this LOOOOOOOOOOOOOOOOOOOL")  // @Autonomous(...) is the other common choice
+@Disabled
+public class HumanRobotProject extends LinearOpMode {
+
+    DcMotor rightFoot;
+    DcMotor leftFoot;
+    /**
+     * The left and right foot motors move the feet. Set power would cause
+     * the feet to slide at a certain speed and set target position sets
+     * an amount of footsteps to move the feet by
+     *
+     * Examples:
+     * 1) leftFoot.setPower(-1) while not using target position will
+     * make the foot slide backwards rather fast
+     *
+     * 2) rightFoot.setTargetPosition(12) followed by rightFoot.setPower(0.1)
+     * will make that foot and that foot only move 12 steps forward very slowly
+     */
+    ColorSensor eyes;
+    /**
+     * Using eyes.red(), eyes.blue(), or eyes.green() will grab how much of that
+     * color we see, so you can compare them to see what color is presented
+     *
+     * Example:
+     * If eyes.green() > eyes.red() && eyes.green() > eyes.blue(), the "robot"
+     * most likely sees green
+     */
+    Servo leftShoulder;
+    Servo rightShoulder;
+
+    /**
+     * Using setPostion(0) will set the arm down by your side, and
+     * setPosition(1) would set the arm straight up
+     */
+
+    @Override
+    public void runOpMode() {
+
+    }
+
+    public void turn(double degrees){
+        /**
+         * this function is empty, but for this activity pretend it will
+         * turn the "robot" by the amount of degrees specified
+         */
+    }
+}
